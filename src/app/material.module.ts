dd88d91a5dd24b19b42c317ffce8720f41b78fc5
Roshan Singh <singh.roshan9718@gmail.com>
2023-07-16 10:59:34 +0530

    added signup and login page

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-
+import {MatDatepickerModule, } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
-  imports: [MatButtonModule,MatFormFieldModule,MatInputModule],
-  exports: [MatButtonModule,MatFormFieldModule,MatInputModule],
+  imports: [MatButtonModule,MatFormFieldModule,MatInputModule,MatDatepickerModule,MatNativeDateModule,MatCheckboxModule],
+  exports: [MatButtonModule,MatFormFieldModule,MatInputModule,MatDatepickerModule,MatNativeDateModule,MatCheckboxModule],
 })
 export class MaterialModule {}
